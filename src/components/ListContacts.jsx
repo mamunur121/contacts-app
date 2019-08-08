11bd96d7ae1d1f75b3490505d83d751e9a90ec39
Md mamunur Rashid <mamun.ruet.ete@gmail.com>
2019-08-08 16:27:06 +0200

    ListComponents updated with more details

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -7,8 +7,23 @@ class ListContacts extends Component {
         const {contacts} = this.props;
         return (
             <ol className='contact-list'>
-                {contacts.map(person => (
-                    <li key={person.id}>{person.name}</li>
+                {contacts.map(person =>(
+                    <li key={person.id} className='contact-list-item'>
+                        <div
+                            className='contact-avatar'
+                            style={{
+                                backgroundImage: `url(${person.avatarURL})`
+                            }}
+                        >
+                        </div>
+                        <div className='contact-details'>
+                            <p>{person.name}</p>
+                            <p>{person.handle}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
                 ))}
             </ol>
         )
