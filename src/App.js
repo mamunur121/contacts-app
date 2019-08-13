fae82095c21598607f90c9f40d9204f5328bf971
Md mamunur Rashid <mamun.ruet.ete@gmail.com>
2019-08-13 08:24:49 +0200

    added the local state

--- src/App.js
+++ src/App.js
@@ -2,31 +2,33 @@ import React from 'react';
 import './App.css';
 import ListContacts from "./components/ListContacts";
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
 
 class App extends React.Component{
+  state = {
+    contacts: [
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ],
+  };
   render() {
     return(
-        <ListContacts contacts={contacts}/>
+        <ListContacts contacts={this.state.contacts}/>
     );
   }
 }
--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -1,33 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListContacts extends Component {
 
-    render() {
-        console.log('props', this.props);
-        const {contacts} = this.props;
-        return (
-            <ol className='contact-list'>
-                {contacts.map(person =>(
-                    <li key={person.id} className='contact-list-item'>
-                        <div
-                            className='contact-avatar'
-                            style={{
-                                backgroundImage: `url(${person.avatarURL})`
-                            }}
-                        >
-                        </div>
-                        <div className='contact-details'>
-                            <p>{person.name}</p>
-                            <p>{person.handle}</p>
-                        </div>
-                        <button className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
+function ListContacts({contacts}) {
+    return(
+        <ol className='contact-list'>
+            {contacts.map(person =>(
+                <li key={person.id} className='contact-list-item'>
+                    <div
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${person.avatarURL})`
+                        }}
+                    >
+                    </div>
+                    <div className='contact-details'>
+                        <p>{person.name}</p>
+                        <p>{person.handle}</p>
+                    </div>
+                    <button className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    );
 }
+// class ListContacts extends Component {
+//
+//     render() {
+//       //  console.log('props', this.props);
+//         const {contacts} = this.props;
+//         return (
+//             <ol className='contact-list'>
+//                 {contacts.map(person =>(
+//                     <li key={person.id} className='contact-list-item'>
+//                         <div
+//                             className='contact-avatar'
+//                             style={{
+//                                 backgroundImage: `url(${person.avatarURL})`
+//                             }}
+//                         >
+//                         </div>
+//                         <div className='contact-details'>
+//                             <p>{person.name}</p>
+//                             <p>{person.handle}</p>
+//                         </div>
+//                         <button className='contact-remove'>
+//                             Remove
+//                         </button>
+//                     </li>
+//                 ))}
+//             </ol>
+//         )
+//     }
+// }
 
 export default ListContacts;
